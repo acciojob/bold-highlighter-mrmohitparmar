8b2d85b4eb3965a023397bb6bbdf33a7c83616a6
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-11-13 22:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function highlight() {
     //Write your code here
-
+	let str = document.querySelectorAll("strong")
+  for (let i = 0; i < str.length; i++) {
+    str[i].style.color = "green";
+  }
 
 }
 
@@ -8,5 +11,13 @@ function highlight() {
 function return_normal() {
     //Write your code here
 
+	let str = document.querySelectorAll("strong")
+  for (let i = 0; i < str.length; i++) {
+    str[i].style.color = "black";
+  }
     
 }
+
+	let para = document.querySelectorAll(".para");
+para.addEvenetListerner("mouseenter",highlight);
+para.addEventListerner("mouseleave", return_normal);
